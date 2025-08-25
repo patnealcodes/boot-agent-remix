7664fe1110cb2457ff319e9ff9065ec516df48d1
patnealcodes <hi@patneal.codes>
2025-08-25 15:53:04 -0400

    move prompt to be provided as an argument

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ from dotenv import load_dotenv
 from google import genai
 from sys import argv
 
+MODEL="gemini-2.5-flash"
+
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-MODEL="gemini-2.5-flash"
+# Unpack prompt & unused args form argv
+prompt, *args = argv[1:] or (None, None)
 
 def main():
-    content = client.models.generate_content(
-        model=MODEL,
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum"
-    )
+    if prompt:
+        content = client.models.generate_content(
+            model=MODEL,
+            contents=prompt
+        )
 
-    print(content.text)
+        print(content.text)
 
-    if content.usage_metadata:
-        print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+        if content.usage_metadata:
+            print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+    else:
+        print("Error: Please include a prompt")
+        exit(1)
 
 if __name__ == "__main__":
     main()
